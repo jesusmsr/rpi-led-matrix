fe79ec14cf484c62f527a29f89d17d75c31cc429
jesusmsr <jsr20xx@gmail.com>
2022-10-06 00:06:55 +0200

    Add input parameter

--- test1.py
+++ test1.py
@@ -9,21 +9,22 @@ class TestDraw(SampleBase):
     
     def __init__(self, *args, **kwargs):
         super(TestDraw, self).__init__(*args, **kwargs)
-        
-    # Configuration for the matrix
-    options = RGBMatrixOptions()
-    options.rows = 32
-    options.cols = 64
-    options.chain_length = 1
-    options.parallel = 1
-    options.hardware_mapping = 'adafruit-hat'
-    options.gpio_slowdown = 2
-
-    offset_canvas = self.matrix.CreateFrameCanvas()
     
-    while True:
-        for x in range (0, self.matrix.width):
-            offset_canvas.SetPixel(x,x,255,255,255)
+    def run(self):
+        # Configuration for the matrix
+        options = RGBMatrixOptions()
+        options.rows = 32
+        options.cols = 64
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = 'adafruit-hat'
+        options.gpio_slowdown = 2
+
+        offset_canvas = self.matrix.CreateFrameCanvas()
+        
+        while True:
+            for x in range (0, self.matrix.width):
+                offset_canvas.SetPixel(x,x,255,255,255)
 
 # Main function
 if __name__ == "__main__":
